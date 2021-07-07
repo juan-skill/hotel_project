e7164eed80a88630afcc528f96c4aa43e845a93a
juan-skill <1294@holbertonschool.com>
2021-07-07 05:18:02 +0000

    [add] install axious and connection to the API 	modified:   web-server/package-lock.json 	modified:   web-server/package.json 	modified:   web-server/src/store/index.js

--- web-server/src/store/index.js
+++ web-server/src/store/index.js
@@ -1,14 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    hotels: [],
+    hotel: null,
   },
   mutations: {
+    setHotels(state, payload) {
+      state.hotels = payload;
+    },
+    setHotel(state, payload) {
+      state.hotel = payload;
+    },
   },
   actions: {
+    async getHotels({ commit }) {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:3000/api/hotels"
+        );
+        if (response.statusText == "OK") {
+            commit("setHotels", response.data);
+        }
+        console.log("helsd");
+      } catch (error) {
+        console.log("holas")
+        console.warn(error);      
+      }
+    },
+    async getHotel({ commit }, hotelID) {     
+        try {
+          console.log(hotelID);
+          const bodyRequest = { hotelID: hotelID };
+          const headersRequest = { "Content-type": "application/json" };
+          const response = await axios.post(
+            `http://127.0.0.1:3000/api/hotels/hotelID`,
+            bodyRequest,
+            { headersRequest }
+          );
+
+          if (response.statusText == "OK") {
+            commit("setHotel", response.data);
+          }
+        } catch (error) {          
+          console.warn(error);
+        }
+      }
   },
   modules: {
   }
